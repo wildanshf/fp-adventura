eeaf6a44906226a422648922a8478f882af1dc28
Wildan <dedan03@yahoo.co.id>
2021-10-26 00:27:44 +0700

    update routes for checkout

--- routes/web.php
+++ routes/web.php
@@ -5,17 +5,38 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\DetailController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\CheckoutController;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\DashboardController;
-use App\Http\Controllers\Admin\TravelPackageController;
 use App\Http\Controllers\Admin\GalleryController;
 use App\Http\Controllers\Admin\TransactionController;
+use App\Http\Controllers\Admin\TravelPackageController;
 
 Route::get('/', [HomeController::class, 'index'])
     ->name('home');
 
 Route::get('/detail/{slug}', [DetailController::class, 'index'])
     ->name('detail');
+
+Route::post('/checkout/{id}', [CheckoutController::class, 'process'])
+    ->name('checkout-process')
+    ->middleware(['auth', 'verified']);
+
+Route::get('/checkout/{id}', [CheckoutController::class, 'index'])
+    ->name('checkout')
+    ->middleware(['auth', 'verified']);
+
+Route::post('/checkout/create/{detail_id}', [CheckoutController::class, 'create'])
+    ->name('checkout-create')
+    ->middleware(['auth', 'verified']);
+
+Route::get('/checkout/remove/{detail_id}', [CheckoutController::class, 'remove'])
+    ->name('checkout-remove')
+    ->middleware(['auth', 'verified']);
+
+Route::get('/checkout/confirm/{id}', [CheckoutController::class, 'success'])
+    ->name('checkout-success')
+    ->middleware(['auth', 'verified']);
 
 Route::prefix('admin')
     ->middleware(['auth', 'isAdmin'])
@@ -29,6 +50,7 @@ Route::prefix('admin')
 });
 
 Route::get('login', [LoginController::class, 'index'])
+    ->name('login')
     ->middleware('guest');
 Route::post('authenticate', [LoginController::class, 'authenticate']);
 Route::post('logout', [LoginController::class, 'logout']);
